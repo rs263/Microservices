9794f83080a513ea8c80c4087f126309b6939cf2
rs263 <rohit.sonar.aem@gmail.com>
2025-01-21 16:34:21 +0530

    changed java version from 17 to 11 and spring from 3 to 2.4.4

--- apigateway/src/main/java/com/apigateway/ApigatewayApplication.java
+++ apigateway/src/main/java/com/apigateway/ApigatewayApplication.java
@@ -2,9 +2,11 @@ package com.apigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-@Enab
+@EnableEurekaClient
+
 public class ApigatewayApplication {
 
 	public static void main(String[] args) {
--- apigateway/src/main/java/com/apigateway/EnableEurekaClient.java
+++
@@ -1,5 +0,0 @@
-package com.apigateway;
-
-public @interface EnableEurekaClient {
-
-}
